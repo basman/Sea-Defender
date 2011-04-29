fc7d68ec3201de7806d1522273217733e350d318
Roman Hoog Antink <rha_github@disconnect.ch>
2011-04-29 19:48:30 +1000

    more debug output in simple-bot

--- bots/simple-bot.pl
+++ bots/simple-bot.pl
@@ -1,4 +1,13 @@
 #!/usr/bin/perl -w
+#
+# Simple bot:
+#	- launches missiles immediately upon receipt of a torpedo message
+#	- pboat side is always the same as side of torpedo's launch position
+#	- does not care about remaining missiles per pboat
+#	- goes for every torpedo, even if it would hit the water
+#	- no internal timing
+#	- no multi-hit strategy
+#
 
 use IO::Socket;
 use strict;
@@ -27,7 +36,7 @@ sub fire_solution($$$$$) {
     my $fireY = abs($toY-$fromY)/2;
     my $side  = $fromX < 0.5 ? 'l' : 'r';
 
-    print STDERR "  target $fireX,$fireY; side='$side'\n";
+    print STDERR "<<  target $fireX,$fireY; side='$side'\n";
 
     return ($fireX, $fireY, $side);
 }
@@ -54,7 +63,7 @@ sub receive($) {
     }
 
     if($event eq "launch_torpedo") {
-	print "torpedo launch at $posX,$posY target=$params{target} speed=$params{velocity}\n";
+	print ">> torpedo launch at $posX,$posY target=$params{target} speed=$params{velocity}\n";
 
 	my ($toX, $toY) = split(/,/, $params{target}, 2);
 	my ($solutionX, $solutionY, $side) = fire_solution($posX, $posY, $toX, $toY, $params{velocity});
@@ -65,6 +74,8 @@ sub receive($) {
 	} else {
 	    $missiles_left_R = $params{missiles_left};
 	}
+    } elsif($event eq 'missile_fired') {
+	print ">> missile_fired at $posX,$posY to $params{destination}, radius=$params{radius}\n";
     }
 }
 
